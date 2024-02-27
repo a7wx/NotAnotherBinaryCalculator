8809bfc7588303e3a07877b3559033d238a18c80
Ryan Downs <dryan@southern.edu>
2024-02-27 16:21:28 -0500

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import BinaryInputBar from './components/BinaryInputBar'
 import DecimalInput from './components/DecimalInput'
+import { Typography } from '@mui/material'
 
 function App() {
   const [decimalValue, setDecimalValue] = useState(0)
 
   return (
+    <>
+    <h1>Not Another Binary Calculator</h1>
+
     <div className='mainBar'>
       <BinaryInputBar totalDigits={8} setDecimalValue={setDecimalValue} decimalValue={decimalValue}/>
       <label>=</label>
       <DecimalInput setDecimalValue={setDecimalValue} decimalValue={decimalValue}/> 
     </div>
+    </>
   )
 }
 
