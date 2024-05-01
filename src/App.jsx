65ea49b2c28afc817e5be19ad45063ca863ea9c7
Ryan Downs <100707977+a7wx@users.noreply.github.com>
2024-05-01 17:06:38 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import BinaryInputBar from './components/BinaryInputBar'
-import DecimalInput from './components/DecimalInput'
-import { Typography } from '@mui/material'
+import ValueRow from './components/ValueRow'
 
 function App() {
-  const [decimalValue, setDecimalValue] = useState(0)
+  const [decimalValues, setDecimalValues] = useState([0, 0, 0])
 
   return (
     <>
     <h1>Not Another Binary Calculator</h1>
-
-    <div className='mainBar'>
-      <BinaryInputBar totalDigits={8} setDecimalValue={setDecimalValue} decimalValue={decimalValue}/>
-      <label>=</label>
-      <DecimalInput setDecimalValue={setDecimalValue} decimalValue={decimalValue}/> 
-    </div>
+    {decimalValues.map((item, index) => 
+    <ValueRow key={index} rowDecimalValue={item} />)}
     </>
   )
 }
